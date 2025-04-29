a41d99c6fb18cdacb98e9525d1fd18db1fb5579a
wafulakelly <wafulakelly899@gmail.com>
2025-04-29 13:07:31 +0300

    Add files via upload

---
+++ intro to JavaScript.js
@@ -0,0 +1,46 @@
+let fullName= "Wafula Kelly simiyu"
+let age=23
+let isStudent=true
+console.log(fullName)
+console.log(typeof fullName )
+console.log(age)
+console.log(typeof age)
+console.log(isStudent)
+console.log(typeof isStudent)
+ console.log()
+
+
+let breakfastcost=100
+let lunchCoast=100
+let transportcost=100
+
+let totalDailyExpense=(breakfastcost+lunchCoast+transportcost)
+console.log(totalDailyExpense)
+console.log(typeof totalDailyExpense)
+ console.log()
+
+let fullName2="Jane Wanjiku"
+let age2="18"
+let hasID="true"
+console.log(typeof fullName)
+console.log(typeof age2)
+console.log(typeof hasID)
+console.log()
+
+
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+
+let userGuess = 10;
+
+
+if (userGuess === secretNumber) {
+  console.log("Correct guess!");
+} else {
+
+  console.log("Wrong guess!");
+}
+
+
+console.log("Type of secretNumber:", typeof secretNumber);
+console.log("Type of userGuess:", typeof userGuess);
+console.log()
